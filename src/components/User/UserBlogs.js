e3fb01b9d149a3924ce752cced1fed0a8f94562d
Purvesh <you@example.com>
2021-07-12 18:29:32 +0530

    added loading feature to userBlogs section

--- src/components/User/UserBlogs.js
+++ src/components/User/UserBlogs.js
@@ -4,9 +4,9 @@ import { getAllUserPost } from "../../API/Post/postapicalls";
 import Card from "../Card";
 import Base from "../core/Base";
 
-
 const UserBlog = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const { user, token } = isAuthenticated();
 
@@ -26,11 +26,23 @@ const UserBlog = () => {
     preload(user._id, token);
   }, []);
 
+  const showLoading = () => {
+    return (
+      <h3
+        className="text-center text-primary pt-4"
+        style={{ display: loading ? "" : "none" }}
+      >
+        Loading...
+      </h3>
+    );
+  };
+
   return (
     <Base className="p-2">
       <div className="text-center">
         <h1 className="text-success">All Blogs By {user.name}</h1>
       </div>
+      {showLoading()}
       <div className="row mt-4">
         {posts.map((post, index) => {
           return <Card key={index} post={post} />;
